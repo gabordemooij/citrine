2b951d942bf54c19c1c4ac061c20deecd0acd650
Gabor de Mooij <mail@gabordemooij.com>
2015-11-04 19:23:27 +0100

    Extend test script.

--- runtests.sh
+++ runtests.sh
@@ -11,7 +11,12 @@ for i in $(find tests -name '*.ctr'); do
 		echo "[$j]"
 		j=$((j+1))
 	else
-		echo "FAIL."
+		echo "FAIL.\n"
+		echo "EXPECTED:\n"
+		echo $expected
+		echo "\n\n\n"
+		echo "BUT GOT:\n"
+		echo $result
 		exit 1
 	fi
 done
