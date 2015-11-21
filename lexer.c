750a8d6ed0a1cba0e44a9815b45cf9cc930b2329
Gabor de Mooij <mail@gabordemooij.com>
2015-11-21 13:10:15 +0100

    Rename variables.

--- lexer.c
+++ lexer.c
@@ -11,7 +11,7 @@
 int ctr_clex_bflmt = 100;
 long ctr_clex_tokvlen = 0; /* length of the string value of a token */
 char* ctr_clex_buffer;
-char* code;
+char* ctr_code;
 char* codePoint;
 char* eofcode;
 char* ctr_clex_oldptr;
@@ -24,10 +24,10 @@ char* ctr_clex_olderptr;
  * Loads program into memory.
  */
 void ctr_clex_load(char* prg) {
-	code = prg;
+	ctr_code = prg;
 	ctr_clex_buffer = malloc(ctr_clex_bflmt);
 	ctr_clex_buffer[0] = '\0';
-	eofcode = (code + ctr_program_length - 1);
+	eofcode = (ctr_code + ctr_program_length - 1);
 }
 
 /**
@@ -55,7 +55,7 @@ long ctr_clex_tok_value_length() {
  * Puts back a token and resets the pointer to the previous one.
  */
 void ctr_clex_putback() {
-	code = ctr_clex_oldptr;
+	ctr_code = ctr_clex_oldptr;
 	ctr_clex_oldptr = ctr_clex_olderptr;
 }
 
@@ -70,73 +70,73 @@ int ctr_clex_tok() {
 	int i, comment_mode;
 	ctr_clex_tokvlen = 0;
 	ctr_clex_olderptr = ctr_clex_oldptr;
-	ctr_clex_oldptr = code;
+	ctr_clex_oldptr = ctr_code;
 	i = 0;
 	comment_mode = 0;
-	c = *code;
-	while(code != eofcode && (isspace(c) || c == '#' || comment_mode)) {
+	c = *ctr_code;
+	while(ctr_code != eofcode && (isspace(c) || c == '#' || comment_mode)) {
 		if (c == '\n') comment_mode = 0;
 		if (c == '#') comment_mode = 1;
-		code ++;
-		c = *code;
+		ctr_code ++;
+		c = *ctr_code;
 	}
-	if (code == eofcode) return CTR_TOKEN_FIN;
-	if (c == '(') { code++; return CTR_TOKEN_PAROPEN; }
-	if (c == ')') { code++; return CTR_TOKEN_PARCLOSE; }
-	if (c == '{') { code++; return CTR_TOKEN_BLOCKOPEN; }
-	if (c == '}') {  code++; return CTR_TOKEN_BLOCKCLOSE; }
-	if (c == '.') { code++; return CTR_TOKEN_DOT; }
-	if (c == ',') { code++; return CTR_TOKEN_CHAIN; }
-	if (c == ':' && (code+1)<eofcode && (*(code+1)=='=')) {
-		code += 2;
+	if (ctr_code == eofcode) return CTR_TOKEN_FIN;
+	if (c == '(') { ctr_code++; return CTR_TOKEN_PAROPEN; }
+	if (c == ')') { ctr_code++; return CTR_TOKEN_PARCLOSE; }
+	if (c == '{') { ctr_code++; return CTR_TOKEN_BLOCKOPEN; }
+	if (c == '}') { ctr_code++; return CTR_TOKEN_BLOCKCLOSE; }
+	if (c == '.') { ctr_code++; return CTR_TOKEN_DOT; }
+	if (c == ',') { ctr_code++; return CTR_TOKEN_CHAIN; }
+	if (c == ':' && (ctr_code+1)<eofcode && (*(ctr_code+1)=='=')) {
+		ctr_code += 2;
 		return CTR_TOKEN_ASSIGNMENT; 
 	}
-	if (c == ':') { code++; return CTR_TOKEN_COLON; }
-	if (c == '^') { code++; return CTR_TOKEN_RET; }
-	if (c == '\'') { code++; return CTR_TOKEN_QUOTE; }
-	if ((c == '-' && (code+1)<eofcode && isdigit(*(code+1))) || isdigit(c)) {
+	if (c == ':') { ctr_code++; return CTR_TOKEN_COLON; }
+	if (c == '^') { ctr_code++; return CTR_TOKEN_RET; }
+	if (c == '\'') { ctr_code++; return CTR_TOKEN_QUOTE; }
+	if ((c == '-' && (ctr_code+1)<eofcode && isdigit(*(ctr_code+1))) || isdigit(c)) {
 		ctr_clex_buffer[i] = c; ctr_clex_tokvlen++;
 		i++;
-		code++;
-		c = *code;
+		ctr_code++;
+		c = *ctr_code;
 		while((isdigit(c))) {
 			ctr_clex_buffer[i] = c; ctr_clex_tokvlen++;
 			i++;
-			code++;
-			c = *code;
+			ctr_code++;
+			c = *ctr_code;
 		}
-		if (c=='.' && (code+1 <= eofcode) && !isdigit(*(code+1))) {
+		if (c=='.' && (ctr_code+1 <= eofcode) && !isdigit(*(ctr_code+1))) {
 			return CTR_TOKEN_NUMBER;
 		}
 		if (c=='.') {
 			ctr_clex_buffer[i] = c; ctr_clex_tokvlen++;
 			i++;
-			code++;
-			c = *code;
+			ctr_code++;
+			c = *ctr_code;
 		}
 		while((isdigit(c))) {
 			ctr_clex_buffer[i] = c; ctr_clex_tokvlen++;
 			i++;
-			code++;
-			c = *code;
+			ctr_code++;
+			c = *ctr_code;
 		}
 		return CTR_TOKEN_NUMBER;
 	}
-	if (strncmp(code, "True", 4)==0){
-		if (CTR_IS_DELIM(*(code + 4))) { 
-			code += 4;
+	if (strncmp(ctr_code, "True", 4)==0){
+		if (CTR_IS_DELIM(*(ctr_code + 4))) { 
+			ctr_code += 4;
 			return CTR_TOKEN_BOOLEANYES;
 		}
 	}
-	if (strncmp(code, "False", 5)==0){
-		if (CTR_IS_DELIM(*(code + 5))) { 
-			code += 5;
+	if (strncmp(ctr_code, "False", 5)==0){
+		if (CTR_IS_DELIM(*(ctr_code + 5))) { 
+			ctr_code += 5;
 			return CTR_TOKEN_BOOLEANNO;
 		}
 	}
-	if (strncmp(code, "Nil", 3)==0){
-		if (CTR_IS_DELIM(*(code + 3))) {
-			code += 3;
+	if (strncmp(ctr_code, "Nil", 3)==0){
+		if (CTR_IS_DELIM(*(ctr_code + 3))) {
+			ctr_code += 3;
 			return CTR_TOKEN_NIL;
 		}
 	}
@@ -146,46 +146,46 @@ int ctr_clex_tok() {
 	 * them without spacing: 1+2 instead of 1 + 2.
 	 */
 	if (c=='+' || c=='-' || c=='/' || c=='*') {
-		code++; ctr_clex_tokvlen = 1; ctr_clex_buffer[i] = c; return CTR_TOKEN_REF;
+		ctr_code++; ctr_clex_tokvlen = 1; ctr_clex_buffer[i] = c; return CTR_TOKEN_REF;
 	}
 
 	/*
 	 * these are also special, they are easy notations for unicode symbols.
 	 * we also return directly because we would like to use them without spaces as well: 1>=2...
 	 */
-	if ((code+1)<eofcode) {
-		if (((char)*(code) == '>') && ((char)*(code+1)=='=')){
-			code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "≥", 3); return CTR_TOKEN_REF;
+	if ((ctr_code+1)<eofcode) {
+		if (((char)*(ctr_code) == '>') && ((char)*(ctr_code+1)=='=')){
+			ctr_code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "≥", 3); return CTR_TOKEN_REF;
 		}
-		if (((char)*(code) == '<') && ((char)*(code+1)=='=')){
-			code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "≤", 3); return CTR_TOKEN_REF;
+		if (((char)*(ctr_code) == '<') && ((char)*(ctr_code+1)=='=')){
+			ctr_code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "≤", 3); return CTR_TOKEN_REF;
 		}
-		if (((char)*(code) == '!') && ((char)*(code+1)=='=')){
-			code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "≠", 3); return CTR_TOKEN_REF;
+		if (((char)*(ctr_code) == '!') && ((char)*(ctr_code+1)=='=')){
+			ctr_code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "≠", 3); return CTR_TOKEN_REF;
 		}
-		if (((char)*(code) == '|') && ((char)*(code+1)=='|')){
-			code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "∨", 3); return CTR_TOKEN_REF;
+		if (((char)*(ctr_code) == '|') && ((char)*(ctr_code+1)=='|')){
+			ctr_code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "∨", 3); return CTR_TOKEN_REF;
 		}
-		if (((char)*(code) == '&') && ((char)*(code+1)=='&')){
-			code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "∧", 3); return CTR_TOKEN_REF;
+		if (((char)*(ctr_code) == '&') && ((char)*(ctr_code+1)=='&')){
+			ctr_code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "∧", 3); return CTR_TOKEN_REF;
 		}
-		if (((char)*(code) == '<') && ((char)*(code+1)=='-')){
-			code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "←", 3); return CTR_TOKEN_REF;
+		if (((char)*(ctr_code) == '<') && ((char)*(ctr_code+1)=='-')){
+			ctr_code +=2; ctr_clex_tokvlen = 3; memcpy(ctr_clex_buffer, "←", 3); return CTR_TOKEN_REF;
 		}
 		/* be very nice, accidental == will be converted to = */
-		if (((char)*(code) == '=') && ((char)*(code+1)=='=')){
-			code +=2; ctr_clex_tokvlen = 1; memcpy(ctr_clex_buffer, "=", 1); return CTR_TOKEN_REF;
+		if (((char)*(ctr_code) == '=') && ((char)*(ctr_code+1)=='=')){
+			ctr_code +=2; ctr_clex_tokvlen = 1; memcpy(ctr_clex_buffer, "=", 1); return CTR_TOKEN_REF;
 		}
 	}
 
 	/* later because we tolerate == as well. */
 	if (c=='=' || c=='>' || c =='<') {
-		code++; ctr_clex_tokvlen = 1; ctr_clex_buffer[i] = c; return CTR_TOKEN_REF;
+		ctr_code++; ctr_clex_tokvlen = 1; ctr_clex_buffer[i] = c; return CTR_TOKEN_REF;
 	}
 
-	if (c == '|' || c == '\\') { code++; return CTR_TOKEN_BLOCKPIPE; }
+	if (c == '|' || c == '\\') { ctr_code++; return CTR_TOKEN_BLOCKPIPE; }
 
-	while(!isspace(c) && CTR_IS_NO_TOK(c) && code!=eofcode
+	while(!isspace(c) && CTR_IS_NO_TOK(c) && ctr_code!=eofcode
 	&& c != '+' && c!='*' && c!='/' && c!='=' && c!='>' && c!='<' && c!='&' /* and c is not one of the special symbols */
 	) {
 		ctr_clex_buffer[i] = c; ctr_clex_tokvlen++;
@@ -194,8 +194,8 @@ int ctr_clex_tok() {
 			printf("[ERROR L001]: Token Buffer Exausted. Tokens may not exceed 100 bytes.");
 			exit(1);
 		}
-		code++;
-		c = *code;
+		ctr_code++;
+		c = *ctr_code;
 	}
 
 	return CTR_TOKEN_REF;
@@ -216,14 +216,14 @@ char* ctr_clex_readstr() {
 	long page = 100; /* 100 byte pages */
 	ctr_clex_tokvlen=0;
 	strbuff = (char*) malloc(memblock);
-	c = *code;
+	c = *ctr_code;
 	escape = 0;
 	beginbuff = strbuff;
 	while((c != '\'' || escape == 1)) {
 		if (c == '\\' && escape == 0) {
 			escape = 1;
-			code++;
-			c = *code;
+			ctr_code++;
+			c = *ctr_code;
 			continue;
 		}
 		ctr_clex_tokvlen ++;
@@ -240,8 +240,8 @@ char* ctr_clex_readstr() {
 		escape = 0;
 		*strbuff = c;
 		strbuff++;
-		code++;
-		c = *code;
+		ctr_code++;
+		c = *ctr_code;
 	}
 	return beginbuff;
 }
