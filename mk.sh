27c85e0b564182fd079d68feabb1f6355cd81c1a
Gabor de Mooij <mail@gabordemooij.com>
2016-02-01 01:00:31 +0100

    Add openbsd makefile and make-selector script mk.

--- mk.sh
+++ mk.sh
@@ -1,3 +1,14 @@
+#MK Script
+#Written by Gabor de Mooij
+#Decides which makefile to use
+OS=$(uname -s)
+if [[ $OS -eq "OpenBSD" ]];then
+	print "using OpenBSD Makefile."
+	make -f makefile.openbsd
+else
+	print "using Linux Makefile."
+	make -f makefile
+fi
 
-make
+#Clear object files
 rm *.o
