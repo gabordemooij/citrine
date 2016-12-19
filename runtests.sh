d22a3420516325f162ccfb649742f6ae1b86725e
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2016-12-20 00:35:56 +0100

    Add test for stdin input.

--- runtests.sh
+++ runtests.sh
@@ -39,7 +39,7 @@ for i in $(find tests -name 'test*.ctr'); do
 	fitem=$i
 	echo -n "$fitem interpret";
 	fexpect="${i%%.ctr}.exp"
-	result=`./ctr ${fitem}`
+	result=`echo "test" | ./ctr ${fitem}`
 	expected=`cat $fexpect`
 	if [ "$result" = "$expected" ]; then
 		echo "[$j]"
