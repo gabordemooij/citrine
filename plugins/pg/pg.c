f7196f8ac5930f68d645a4e13bca0c682db24fde
gabor <gabordemooij@gaborsoftware.nl>
2018-02-25 05:27:09 +0100

    Refined PostgreSQL plugin.

--- plugins/pg/pg.c
+++ plugins/pg/pg.c
@@ -4,34 +4,38 @@
 
 #include "../../citrine.h"
 
+
+#define CTR_OBJECT_RESOURCE_PG 40
+
 ctr_object* ctr_pg_new(ctr_object* myself, ctr_argument* argumentList) {
-	ctr_object* pgInstance = ctr_internal_create_object(CTR_OBJECT_TYPE_OTOBJECT);
+	PGconn *psql;
+	char* connectionString = ctr_heap_allocate_cstring(argumentList->object);
+	psql = PQconnectdb(connectionString);
+	ctr_heap_free(connectionString);
+	if (!psql) {
+		CtrStdFlow = ctr_build_string_from_cstring("PostgreSQL: Unable to establish a connection.");
+		return CtrStdNil;
+	}
+	if (PQstatus(psql) != CONNECTION_OK) {
+		CtrStdFlow = ctr_build_string_from_cstring("PostgreSQL: Connection not OK.");
+		return CtrStdNil;
+	}
+	ctr_object* pgInstance = ctr_internal_create_object(CTR_OBJECT_TYPE_OTEX);
 	pgInstance->link = myself;
-	ctr_internal_object_set_property(
-		pgInstance, 
-		ctr_build_string_from_cstring( "connection" ),
-		argumentList->object,
-		CTR_CATEGORY_PRIVATE_PROPERTY
-	);
+	pgInstance->info.type = CTR_OBJECT_TYPE_OTEX;
+	ctr_resource* rs = ctr_heap_allocate( sizeof(ctr_resource) );
+	rs->type = CTR_OBJECT_RESOURCE_PG;
+	rs->ptr = psql;
+	pgInstance->value.rvalue = rs;
 	return pgInstance;	
 }
 
+
 ctr_object* ctr_pg_query(ctr_object* myself, ctr_argument* argumentList) {
-	char* connection = ctr_heap_allocate_cstring(
-		ctr_internal_object_find_property(
-			myself,
-			ctr_build_string_from_cstring("connection"),
-			CTR_CATEGORY_PRIVATE_PROPERTY
-		)
-	);
-	PGconn *psql;
-	psql = PQconnectdb(connection);
-	if (!psql) {
-		fprintf(stderr, "libpq error: PQconnectdb returned NULL.\n\n");
+	if (myself->value.rvalue == NULL) {
+		return myself;
 	}
-	if (PQstatus(psql) != CONNECTION_OK) {
-		fprintf(stderr, "libpq error: PQstatus(psql) != CONNECTION_OK\n\n");
-	}
+	PGconn* psql;
 	char* command = ctr_heap_allocate_cstring( ctr_internal_cast2string( argumentList->object ) );
 	int resultFormat = 0;
 	int nParams = 0;
@@ -45,15 +49,20 @@ ctr_object* ctr_pg_query(ctr_object* myself, ctr_argument* argumentList) {
 	ctr_object* params = argumentList->next->object;
 	ctr_object* param;
 	int len = (int) ctr_internal_cast2number( ctr_array_count( params, NULL ) )->value.nvalue;
+	psql = (PGconn*) myself->value.rvalue->ptr;
 	paramValues = ctr_heap_allocate( len );
 	for(q=0; q<len; q++) {
 		b->object = ctr_build_number_from_float((ctr_number)q);
 		param = ctr_array_get(params, b);
 		nParams++;
-		paramValue = ctr_heap_allocate_cstring(param);
+		paramValue = ctr_heap_allocate_cstring(ctr_internal_cast2string(param));
 		*(paramValues + q) = paramValue;
 	}
 	PGresult* res = PQexecParams(psql, command, nParams, NULL, (const char* const*) paramValues, NULL, NULL, resultFormat);
+	for(q=0; q<len; q++) {
+		paramValue = *(paramValues + q);
+		ctr_heap_free(paramValue);
+	}
 	int rows = PQntuples(res);
 	int cols = PQnfields(res);
 	ctr_object* cells;
@@ -75,14 +84,29 @@ ctr_object* ctr_pg_query(ctr_object* myself, ctr_argument* argumentList) {
 		a->object = cells;
 		ctr_array_push(crows, a);
 	}
-	PQfinish( psql );
+	ctr_heap_free(a);
+	ctr_heap_free(b);
+	ctr_heap_free(command);
+	ctr_heap_free( paramValues );
 	return crows;
 }
 
+ctr_object* ctr_pg_close(ctr_object* myself, ctr_argument* argumentList) {
+	if (myself->value.rvalue == NULL) {
+		return myself;
+	}
+	PQfinish( (PGconn*) myself->value.rvalue->ptr );
+	ctr_heap_free(myself->value.rvalue);
+	myself->value.rvalue = NULL;
+	return myself;
+}
+
 void begin(){
-	ctr_object*  pgObject = ctr_internal_create_object(CTR_OBJECT_TYPE_OTOBJECT);
+	ctr_object*  pgObject = ctr_internal_create_object(CTR_OBJECT_TYPE_OTEX);
+	pgObject->value.rvalue = NULL;
 	ctr_internal_create_func(pgObject, ctr_build_string_from_cstring( "new:" ), &ctr_pg_new );
 	ctr_internal_create_func(pgObject, ctr_build_string_from_cstring( "query:parameters:" ), &ctr_pg_query );
+	ctr_internal_create_func(pgObject, ctr_build_string_from_cstring( "close" ), &ctr_pg_close );
 	ctr_internal_object_add_property(CtrStdWorld, ctr_build_string_from_cstring( "Pg" ), pgObject, CTR_CATEGORY_PUBLIC_PROPERTY);
 	pgObject->link = CtrStdObject;
 	pgObject->info.sticky = 1;
