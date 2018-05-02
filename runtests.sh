2881f651f613f60d461103b9ae478edc170ad6f9
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2018-05-02 08:30:47 +0200

    Test various combinations.

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
-
-set -x
-set -v
+#!/bin/bash
+#set -x
+#set -v
 
 OS=`uname -s`
 LDFLAGS='-shared'
@@ -84,10 +83,7 @@ gcc -shared -o libctrpassword.so crypt.o -L/usr/local/lib/ -lsodium
 cd ..
 cd ..
 cp plugins/crypt/libctrpassword.so mods/password/libctrpassword.so
-
-
 ./mk.sh
-
 j=1
 for i in $(find tests -name 'test*.ctr'); do
 	touch /tmp/a
@@ -96,16 +92,51 @@ for i in $(find tests -name 'test*.ctr'); do
 	touch /tmp/d
 	touch /tmp/transl.ctr
 	fitem=$i
-	echo -n "$fitem interpret";
+	echo "[suite: $fitem]";
 	fexpect="${i%%.ctr}.exp"
-	result1=`echo "test" | ./ctr ${fitem} 1>/tmp/a 2>/tmp/b`
-	trans=`./ctr -t ennl.dict ${fitem} > /tmp/transl.ctr`
-	result2=`echo "test" | ./ctrnl /tmp/transl.ctr 1>/tmp/c 2>/tmp/d`
-	result=`cat /tmp/a /tmp/b`
-	resultB=`cat /tmp/c /tmp/d`
+	#test for every GC mode (0/1/4/8/9/12) mode x every language (EN/NL)
+	rm tests/runner1.ctr ; echo "Broom mode: 0." > tests/runner1.ctr ; cat ${fitem} >> tests/runner1.ctr
+	rm tests/runner2.ctr ; echo "Broom mode: 1." > tests/runner2.ctr ; cat ${fitem} >> tests/runner2.ctr
+	rm tests/runner3.ctr ; echo "Broom mode: 4." > tests/runner3.ctr ; cat ${fitem} >> tests/runner3.ctr
+	rm tests/runner4.ctr ; echo "Broom mode: 8." > tests/runner4.ctr ; cat ${fitem} >> tests/runner4.ctr
+	rm tests/runner5.ctr ; echo "Broom mode: 9." > tests/runner5.ctr ; cat ${fitem} >> tests/runner5.ctr
+	rm tests/runner6.ctr ; echo "Broom mode: 12." > tests/runner6.ctr ; cat ${fitem} >> tests/runner6.ctr
+	echo "[translating...]"
+	rm tests/runner7.ctr ; ./ctr -t ennl.dict tests/runner1.ctr > tests/runner7.ctr
+	rm tests/runner8.ctr ; ./ctr -t ennl.dict tests/runner2.ctr > tests/runner8.ctr
+	rm tests/runner9.ctr ; ./ctr -t ennl.dict tests/runner3.ctr > tests/runner9.ctr
+	rm tests/runner10.ctr ; ./ctr -t ennl.dict tests/runner4.ctr > tests/runner10.ctr
+	rm tests/runner11.ctr ; ./ctr -t ennl.dict tests/runner5.ctr > tests/runner11.ctr
+	rm tests/runner12.ctr ; ./ctr -t ennl.dict tests/runner6.ctr > tests/runner12.ctr
+	echo "[running...]";
+	echo "test" | ./ctr ${fitem} 1>/tmp/a0 2>/tmp/b0
+	echo "test" | ./ctr tests/runner1.ctr 1>/tmp/a1 2>/tmp/b1
+	echo "test" | ./ctr tests/runner2.ctr 1>/tmp/a2 2>/tmp/b2
+	echo "test" | ./ctr tests/runner3.ctr 1>/tmp/a3 2>/tmp/b3
+	echo "test" | ./ctr tests/runner4.ctr 1>/tmp/a4 2>/tmp/b4
+	echo "test" | ./ctr tests/runner5.ctr 1>/tmp/a5 2>/tmp/b5
+	echo "test" | ./ctr tests/runner6.ctr 1>/tmp/a6 2>/tmp/b6
+	echo "test" | ./ctrnl tests/runner7.ctr 1>/tmp/a7 2>/tmp/b7
+	echo "test" | ./ctrnl tests/runner8.ctr 1>/tmp/a8 2>/tmp/b8
+	echo "test" | ./ctrnl tests/runner9.ctr 1>/tmp/a9 2>/tmp/b9
+	echo "test" | ./ctrnl tests/runner10.ctr 1>/tmp/a10 2>/tmp/b10
+	echo "test" | ./ctrnl tests/runner11.ctr 1>/tmp/a11 2>/tmp/b11
+	echo "test" | ./ctrnl tests/runner12.ctr 1>/tmp/a12 2>/tmp/b12
+	result[0]=`cat /tmp/a0 /tmp/b0`
+	result[1]=`cat /tmp/a1 /tmp/b1`
+	result[2]=`cat /tmp/a2 /tmp/b2`
+	result[3]=`cat /tmp/a3 /tmp/b3`
+	result[4]=`cat /tmp/a4 /tmp/b4`
+	result[5]=`cat /tmp/a5 /tmp/b5`
+	result[6]=`cat /tmp/a6 /tmp/b6`
+	result[7]=`cat /tmp/a7 /tmp/b7`
+	result[8]=`cat /tmp/a8 /tmp/b8`
+	result[9]=`cat /tmp/a9 /tmp/b9`
+	result[10]=`cat /tmp/a10 /tmp/b10`
+	result[11]=`cat /tmp/a11 /tmp/b11`
+	result[12]=`cat /tmp/a12 /tmp/b12`
 	expected=`cat $fexpect`
-	if [ "$result" = "$expected" ]; then
-		echo "[$j]"
+	if [ "${result[0]}" = "$expected" ]; then
 		j=$((j+1))
 	else
 		echo "FAIL."
@@ -113,31 +144,27 @@ for i in $(find tests -name 'test*.ctr'); do
 		echo $expected
 		echo ""
 		echo "BUT GOT:"
-		echo $result
+		echo $result[0]
 		exit 1
 	fi
-
-	directive=`head -n1 ${fitem}`
-	if [ "$directive" != "#SINGLE_LANGUAGE" ]; then
-		if [ "$resultB" = "$expected" ]; then
-			echo "[$j]"
-			j=$((j+1))
-		else
-			echo "FAIL."
-			echo "EXPECTED:"
-			echo $expected
-			echo ""
-			echo "BUT GOT:"
-			echo $resultB
-			exit 1
-		fi
+	directive=`head -n1 $fitem`
+    if [ "$directive" != "#SINGLE_LANGUAGE" ]; then
+		for q in {1..12}
+		do
+			if [ "${result[$q]}" = "$expected" ]; then
+				echo "[$j]"
+				j=$((j+1))
+			else
+				echo "FAIL."
+				echo "EXPECTED:"
+				echo $expected
+				echo ""
+				echo "BUT GOT:"
+				echo "${result[$q]}"
+				exit 1
+			fi
+		done
 	fi
-	rm /tmp/a
-	rm /tmp/b
-	rm /tmp/c
-	rm /tmp/d
-	rm /tmp/transl.ctr
-	headline=$(head -n 1 $fitem)
 done
 echo ""
 echo "All tests passed."
