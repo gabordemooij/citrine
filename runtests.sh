13e53a1b84f7d7c405b2c07af910c9196c22b1dd
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2018-05-28 07:51:19 +0200

    Remove Pg, Password, Curl from test.

--- runtests.sh
+++ runtests.sh
@@ -13,21 +13,21 @@ find . -name "*.so" -exec rm {} +
 #For plugin test, compile Percolator plugin
 rm plugins/percolator/libctrpercolator.so
 rm mods/percolator/libctrpercolator.so
-cd plugins/percolator;
-cc -c percolator.c -Wall -Werror -fPIC -o percolator.o
-cc ${LDFLAGS} -o libctrpercolator.so percolator.o
-cd ..
-cd ..
-cp plugins/percolator/libctrpercolator.so mods/percolator/libctrpercolator.so
+#cd plugins/percolator;
+#cc -c percolator.c -Wall -Werror -fPIC -o percolator.o
+#cc ${LDFLAGS} -o libctrpercolator.so percolator.o
+#cd ..
+#cd ..
+#cp plugins/percolator/libctrpercolator.so mods/percolator/libctrpercolator.so
 
 
-rm plugins/percolator/libctrpercolator.so
-cd plugins/percolator;
-cc -DlangNL -c percolator.c -Wall -Werror -fPIC -o koffiezetter.o
-cc ${LDFLAGS} -o libctrkoffiezetter.so koffiezetter.o
-cd ..
-cd ..
-cp plugins/percolator/libctrkoffiezetter.so mods/koffiezetter/libctrkoffiezetter.so
+#rm plugins/percolator/libctrpercolator.so
+#cd plugins/percolator;
+#cc -DlangNL -c percolator.c -Wall -Werror -fPIC -o koffiezetter.o
+#cc ${LDFLAGS} -o libctrkoffiezetter.so koffiezetter.o
+#cd ..
+#cd ..
+#cp plugins/percolator/libctrkoffiezetter.so mods/koffiezetter/libctrkoffiezetter.so
 
 
 #request test
@@ -54,35 +54,36 @@ cp plugins/jsmn/libctrjsmn.so mods/json/libctrjson.so
 rm plugins/curl/src/libctrcurl.so
 rm plugins/curl/src/curl.o
 rm mods/curl/libctrcurl.so
-cd plugins/curl/src;
-gcc -DCTRPLUGIN_CURL_CURLOPT_URL -c curl.c -Wall	-Werror -fpic -o curl.o
-gcc -shared -o libctrcurl.so curl.o -lcurl
-cd ..
-cd ..
-cd ..
-cp plugins/curl/src/libctrcurl.so mods/curl/libctrcurl.so
+#cd plugins/curl/src;
+#gcc -DCTRPLUGIN_CURL_CURLOPT_URL -c curl.c -Wall	-Werror -fpic -o curl.o
+#gcc -shared -o libctrcurl.so curl.o -lcurl
+#cd ..
+#cd ..
+#cd ..
+#cp plugins/curl/src/libctrcurl.so mods/curl/libctrcurl.so
 
 #pg test
 rm plugins/pg/libctrpg.so
 rm plugins/pg/pg.o
 rm mods/pg/libctrpg.so
-cd plugins/pg;
-gcc -c pg.c -Wall -I/usr/include/postgresql/ -lpq -Werror -fpic -o pg.o
-gcc -shared -o libctrpg.so pg.o -I/usr/include/postgresql/ -lpq
-cd ..
-cd ..
-cp plugins/pg/libctrpg.so mods/pg/libctrpg.so
+#cd plugins/pg;
+#gcc -c pg.c -Wall -I/usr/include/postgresql/ -lpq -Werror -fpic -o pg.o
+#gcc -shared -o libctrpg.so pg.o -I/usr/include/postgresql/ -lpq
+#cd ..
+#cd ..
+#cp plugins/pg/libctrpg.so mods/pg/libctrpg.so
 
 #sodium
 rm plugins/crypt/libctrpassword.so
 rm plugins/crypt/crypt.o
 rm mods/password/libctrpassword.so
-cd plugins/crypt;
-gcc -c crypt.c -Wall -I/usr/local/include/  -Werror -fpic -o crypt.o
-gcc -shared -o libctrpassword.so crypt.o -L/usr/local/lib/ -lsodium
-cd ..
-cd ..
-cp plugins/crypt/libctrpassword.so mods/password/libctrpassword.so
+#cd plugins/crypt;
+#gcc -c crypt.c -Wall -I/usr/local/include/  -Werror -fpic -o crypt.o
+#gcc -shared -o libctrpassword.so crypt.o -L/usr/local/lib/ -lsodium
+#cd ..
+#cd ..
+#cp plugins/crypt/libctrpassword.so mods/password/libctrpassword.so
+
 ./mk.sh
 j=1
 for i in $(find tests -name 'test*.ctr'); do
