02db13f1367d9a83d381326fddc2d1bd7f13c95a
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2018-06-06 08:00:39 +0200

    Forgot to add plugin dicts.

---
+++ plugins/jsmn/i18n/en/dictionary.h
@@ -0,0 +1,4 @@
+#define CTR_DICT_JSON_TO_OBJECT_SET "to object:"
+#define CTR_DICT_PLUGIN_JSON         "Json"
+
+
---
+++ plugins/jsmn/i18n/nl/dictionary.h
@@ -0,0 +1,2 @@
+#define CTR_DICT_JSON_TO_OBJECT_SET "naar object:"
+#define CTR_DICT_PLUGIN_JSON        "Jsonnl"
