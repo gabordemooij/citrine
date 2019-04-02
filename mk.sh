74cf5a518422928d1113b89c1801c2ac4535999e
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2019-04-02 09:14:44 +0200

    Gebruik generieke Citrine in plaats van US Citrine voor het schrijven van de woordenboeken.

--- mk.sh
+++ mk.sh
@@ -14,7 +14,7 @@ do
 	make -f $MAKEFILE clean
 	make -f $MAKEFILE all
 	rm dict/en${ISO}.dict
-	bin/${OS}/ctrus -g i18n/us/dictionary.h i18n/${ISO}/dictionary.h > dict/en${ISO}.dict
+	bin/${OS}/ctr -g i18n/us/dictionary.h i18n/${ISO}/dictionary.h > dict/en${ISO}.dict
 	cat i18n/${ISO}/extra.dict >> dict/en${ISO}.dict
 done
 
