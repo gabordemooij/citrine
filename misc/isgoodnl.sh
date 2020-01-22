bbbfd15909f6b3ebd3c0f821245f17ab596b3163
Gabor de Mooij <gabordemooij@gmail.com>
2020-01-22 20:40:45 +0100

    Adjust isgoodnl script.

--- misc/isgoodnl.sh
+++ misc/isgoodnl.sh
@@ -3,8 +3,8 @@ rm /tmp/a
 rm /tmp/b
 touch /tmp/a
 touch /tmp/b
-./ctr -t ennl.dict $1 > /tmp/r.ctr
-result1=`./ctrnl /tmp/r.ctr 1>/tmp/a 2>/tmp/b`
+./ctr -t dict/ennl.dict $1 > /tmp/r.ctr
+result1=`./bin/Linux/ctrnl /tmp/r.ctr 1>/tmp/a 2>/tmp/b`
 result=`cat /tmp/a /tmp/b`
 dest="${1:0: -4}nl.exp"
 touch $dest
