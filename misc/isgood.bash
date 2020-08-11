85250ac69d53fdd2efe454f96e3383d05e7ad90f
Gabor de Mooij <gabordemooij@gmail.com>
2020-08-11 23:33:51 +0200

    isgood script now uses XX as default language.

--- misc/isgood.bash
+++ misc/isgood.bash
@@ -3,7 +3,7 @@ rm /tmp/a
 rm /tmp/b
 touch /tmp/a
 touch /tmp/b
-result1=`./bin/Linux/ctrus $1 1>/tmp/a 2>/tmp/b`
+result1=`./bin/Linux/ctrxx $1 1>/tmp/a 2>/tmp/b`
 result=`cat /tmp/a /tmp/b`
 dest="${1:0: -4}.exp"
 touch $dest
