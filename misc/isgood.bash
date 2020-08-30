5bd049be423e7daf5601dfbf75943c1c738fa870
Gabor de Mooij <gabordemooij@gmail.com>
2020-08-30 22:11:44 +0200

    Remove low level functionality from language.

--- misc/isgood.bash
+++ misc/isgood.bash
@@ -5,7 +5,7 @@ touch /tmp/a
 touch /tmp/b
 result1=`./bin/Linux/ctrxx $1 1>/tmp/a 2>/tmp/b`
 result=`cat /tmp/a /tmp/b`
-dest="${1:0: -4}.exp"
+dest="tests/exp/xx2/test${1:10:4}.exp"
 touch $dest
 echo "$result" > $dest
 echo "Stored result of program in expectation file."
