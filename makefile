c0f2b8c1539a846aae212dad343ca6e2bbf11ce0
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2021-03-20 18:33:04 +0100

    Merged new README, make script and make file from Tirifto.

--- makefile
+++ makefile
@@ -1,17 +1,19 @@
 CFLAGS = -g -mtune=native -Wall -D CTRLANG=${ISO}
 OBJS = siphash.o utf8.o memory.o util.o base.o collections.o file.o system.o \
        world.o lexer.o parser.o walker.o translator.o citrine.o
+prefix ?= /usr
 
 .SUFFIXES:	.o .c
 
 all:ctr
 
-install: ctr
-	cp ./ctr /usr/bin/ctr
+install:
+	install -d $(DESTDIR)$(prefix)/bin $(DESTDIR)$(prefix)/share/fonts/citrine
+	install ./bin/$(shell uname -s)/ctr* $(DESTDIR)$(prefix)/bin
+	install ./fonts/Citrine.ttf          $(DESTDIR)$(prefix)/share/fonts/citrine
 
 ctr:	$(OBJS)
 	$(CC) $(OBJS) -g -rdynamic -lm -ldl -o ctr
-	cp ctr bin/${OS}/ctr
 	cp ctr bin/${OS}/ctr${ISO}
 
 .c.o:
--- mk.sh
+++ mk.sh
@@ -6,18 +6,28 @@ else
 	MAKEFILE=makefile
 fi
 echo "USING: ${MAKEFILE}"
-for ISO in $(ls i18nsel)
+
+#Get list of ISO codes to build.
+ISOs=$(ls i18nsel)
+if [ $# -gt 0 ]; then
+	ISOs=$@
+fi
+
+#Run make for every ISO code.
+for ISO in $ISOs
 do
-	echo $ISO
+	echo ; echo $ISO
 	export ISO
 	export OS
 	make -f $MAKEFILE clean
 	make -f $MAKEFILE all
-	rm dict/xx${ISO}.dict
-	bin/${OS}/ctr -g i18n/xx/dictionary.h i18n/${ISO}/dictionary.h > dict/xx${ISO}.dict
-	bin/${OS}/ctr -g i18n/${ISO}/dictionary.h i18n/xx/dictionary.h > dict/${ISO}xx.dict
+	if [ -f "dict/xx${ISO}.dict" ]; then
+		rm   dict/xx${ISO}.dict
+	fi
+	./ctr -g i18n/xx/dictionary.h i18n/${ISO}/dictionary.h > dict/xx${ISO}.dict
+	./ctr -g i18n/${ISO}/dictionary.h i18n/xx/dictionary.h > dict/${ISO}xx.dict
 	cat i18n/${ISO}/extra.dict >> dict/xx${ISO}.dict
 done
 
-#Clear object files
-rm *.o
+#Clear object files and binary
+rm *.o ctr
