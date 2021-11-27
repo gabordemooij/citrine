5669dd4b2aebf0416fe4cda599efcf4293df268c
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2021-11-27 18:06:44 +0100

    Update Downloadmaker-script.

--- misc/downloadmaker.sh
+++ misc/downloadmaker.sh
@@ -1,5 +1,5 @@
 OS="linux"
-V="0.9.2"
+V="0.9.5"
 for FILE in $(ls downloads/linux)
 do
 	echo "<li><a href=\"/downloads/iso/linux/${FILE}\">${FILE}</a></li>"
@@ -14,6 +14,14 @@ do
 done
 for FILE in $(ls downloads/windows32)
 do
-        echo "<li><a href=\"/downloads/iso/windows32/${FILE}\">${FILE}</a></li>"
+	echo "<li><a href=\"/downloads/iso/windows32/${FILE}\">${FILE}</a></li>"
+done
+for FILE in $(ls downloads/windows64)
+do
+	echo "<li><a href=\"/downloads/iso/windows64/${FILE}\">${FILE}</a></li>"
+done
+for FILE in $(ls downloads/haiku)
+do
+	echo "<li><a href=\"/downloads/iso/haiku/${FILE}\">${FILE}</a></li>"
 done
 
