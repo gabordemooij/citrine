5b17d7dfa553603f36c049ebc026990180ea375a
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2023-10-18 23:36:40 +0200

    Add NL/RU Asset/Extra.

---
+++ plugins/media/i18n/ru/assets.sh
@@ -0,0 +1,30 @@
+rm -rf /tmp/translated_assets
+mkdir /tmp/translated_assets
+cp plugins/media/examples/persoon.png /tmp/translated_assets/человек.png
+cp plugins/media/examples/obstakel.png /tmp/translated_assets/препятствие.png
+cp plugins/media/examples/grasveld.png /tmp/translated_assets/лужайка.png
+cp plugins/media/examples/zee.png /tmp/translated_assets/море.png
+cp plugins/media/examples/konĳn.png /tmp/translated_assets/кролик.png
+cp plugins/media/examples/eiland.png /tmp/translated_assets/остров.png
+cp plugins/media/examples/achtergrond.png /tmp/translated_assets/фоновый.png
+cp plugins/media/examples/boot.png /tmp/translated_assets/лодка.png
+cp plugins/media/examples/canvas.png /tmp/translated_assets/холст.png
+cp plugins/media/examples/tekstveld.png /tmp/translated_assets/текстовое_поле.png
+cp plugins/media/examples/kader.png /tmp/translated_assets/этикетка.png
+cp plugins/media/examples/knop.png /tmp/translated_assets/кнопье.png
+cp plugins/media/examples/lettertype.ttf /tmp/translated_assets/тип_шрифта.ttf
+cp plugins/media/examples/schaar.png /tmp/translated_assets/ножницы.png
+cp plugins/media/examples/papieren.png /tmp/translated_assets/документ.png
+cp plugins/media/examples/klembord.png /tmp/translated_assets/буфер_обмена.png
+cp plugins/media/examples/bezem.png /tmp/translated_assets/метла.png
+cp plugins/media/examples/boek.png /tmp/translated_assets/книга.png
+cp plugins/media/examples/tandwielen.png /tmp/translated_assets/шестерни.png
+cp plugins/media/examples/kist.png /tmp/translated_assets/грудь.png
+cp plugins/media/examples/schrĳfmachine.png /tmp/translated_assets/пишущая_машинка.png
+cp plugins/media/examples/legenda.png /tmp/translated_assets/легенда.png
+cp plugins/media/examples/code.png /tmp/translated_assets/код.png
+cp plugins/media/examples/toets.png /tmp/translated_assets/клавиша.png
+cp plugins/media/examples/__?__.png /tmp/translated_assets/
+cp plugins/media/examples/geluidje.ogg /tmp/translated_assets/звук.ogg
+
+
