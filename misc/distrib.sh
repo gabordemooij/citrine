ab89c84877619228808af510cab881a30605408a
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2024-09-01 20:57:39 +0200

    Update distrib.

--- misc/distrib.sh
+++ misc/distrib.sh
@@ -18,7 +18,9 @@ mkdir dist/Linux
 mkdir dist/Linux/ISO
 mkdir dist/Linux/OUT
 
-declare -a langs=("nl" "en_ascii" "ru" "en" "hi" "fr" "no" "pt_br" "uz" "pl" "cs" "id" "zh2")
+VERSION="1_0_0beta2"
+
+declare -a langs=("nl" "en" "de" "fr" "no" "ru" "cs" "it" "hi" "pt_br" "uz" "pl" "id" "zh2")
 for lang in "${langs[@]}"
 do
 
@@ -32,9 +34,7 @@ ISO="$lang" PACKAGE="media" NAME="libctrmedia.dll" CC=x86_64-w64-mingw32-gcc-win
 # Create dictionary
 ./bin/Linux/ctrnl -g i18n/nl/dictionary.h i18n/$lang/dictionary.h > /tmp/dict_general.dict
 ./bin/Linux/ctrnl -g plugins/media/i18n/nl/media.h plugins/media/i18n/$lang/media.h > /tmp/dict_media.dict
-# Add dictionary for examples 1-6 and 7 (editor)
-cat /tmp/dict_general.dict /tmp/dict_media.dict plugins/media/i18n/$lang/extra.dict > /tmp/dict_all.dict
-
+cat /tmp/dict_general.dict /tmp/dict_media.dict > /tmp/dict_all.dict
 
 # (Re-)Create folder for lang specific distribution
 rm -rf dist/Win64/ISO/$lang
@@ -44,27 +44,14 @@ mkdir dist/Win64/ISO/$lang/mods/media
 # Add executable
 cp bin/Win64/ctr$lang.exe dist/Win64/ISO/$lang/
 # Add dynamic libraries
-cp plugins/media/*.dll dist/Win64/ISO/$lang/
+cp mods/media/dll64/*.dll dist/Win64/ISO/$lang/
 cp plugins/media/libctrmedia.dll dist/Win64/ISO/$lang/mods/media/
 
-
-# Translate assets with script
-source plugins/media/i18n/$lang/assets.sh
-# Add assets
-cp /tmp/translated_assets/* dist/Win64/ISO/$lang/
 # Add shortcut icon
 cp plugins/media/assets/picto.ico dist/Win64/ISO/$lang/pictogram.ico 
 
 # Translate examples
-./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/examples/__1__ > dist/Win64/ISO/$lang/__1__ 2>/tmp/err1.log
-./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/examples/__2__ > dist/Win64/ISO/$lang/__2__ 2>/tmp/err2.log
-./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/examples/__3__ > dist/Win64/ISO/$lang/__3__ 2>/tmp/err3.log
-./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/examples/__4__ > dist/Win64/ISO/$lang/__4__ 2>/tmp/err4.log
-./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/examples/__5win__ > dist/Win64/ISO/$lang/__5__ 2>/tmp/err5win.log
-./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/examples/__6__ > dist/Win64/ISO/$lang/__6__ 2>/tmp/err6.log
-./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/examples/__7__ > dist/Win64/ISO/$lang/__7__ 2>/tmp/err7.log
-./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/examples/__7ai__ > dist/Win64/ISO/$lang/__7ai__ 2>/tmp/err7.log
-./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/examples/client > dist/Win64/ISO/$lang/client 2>/tmp/errclient.log
+./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/tests/test1.ctr > dist/Win64/ISO/$lang/test1.ctr 2>/tmp/err1.log
 
 # Copy assets to setup creator work dir
 cp plugins/media/assets/* ~/.wine/drive_c/InnoSetupSourceDir/
@@ -76,30 +63,30 @@ cp -R dist/Win64/ISO/$lang ~/.wine/drive_c/InnoSetupSourceDir/dist
 
 # Copy setup-creator script to work dir
 suffix="$(echo "$lang" | tr 'a-z' 'A-Z')"
-sed -e "s/ctrnl/ctr$lang/g" -e "s/CitrineNL/Citrine$suffix/" plugins/media/citrine.iss > ~/.wine/drive_c/InnoSetupSourceDir/citrine.iss
+sed -e "s/096/$VERSION/g" -e "s/ctrnl/ctr$lang/g" -e "s/CitrineNL/Citrine$suffix/" plugins/media/citrine.iss > ~/.wine/drive_c/InnoSetupSourceDir/citrine.iss
 
 # Start setup-creator
 wine "C:\\Program Files (x86)\\Inno Setup 6\\ISCC.exe" "C:\\InnoSetupSourceDir\\citrine.iss"
 
 # Copy to output dir
 mkdir -p dist/Win64/OUT/$lang/
-cp ~/.wine/drive_c/InnoSetupSourceDir/Output/Citrine096.exe dist/Win64/OUT/$lang/
+cp ~/.wine/drive_c/InnoSetupSourceDir/Output/Citrine${VERSION}.exe dist/Win64/OUT/$lang/
 
 # Zip it
 cd dist/Win64/OUT/$lang/
-zip Citrine096.zip Citrine096.exe
-rm Citrine096.exe
+zip Citrine${VERSION}.zip Citrine${VERSION}.exe
+rm Citrine${VERSION}.exe
 cd ../../../../
 
 
 # Use the Win64 output as template
 cp -R dist/Win64/ISO/$lang dist/Linux/ISO/$lang
-./bin/Linux/ctrnl -t /tmp/dict_all.dict plugins/media/examples/__5__ > dist/Linux/ISO/$lang/__5__ 2>/tmp/err5.log
 mkdir -p dist/Linux/OUT/$lang
 
 # Compile for Linux
 OS="Linux" ISO="$lang" make -f makefile clean
 OS="Linux" ISO="$lang" make -f makefile
+OS="Linux" ISO="$lang" PACKAGE="media" NAME="libctrmedia.so" make -f makefile clean
 OS="Linux" ISO="$lang" PACKAGE="media" NAME="libctrmedia.so" make -f makefile plugin
 
 
@@ -111,7 +98,7 @@ rm dist/Linux/ISO/$lang/*.dll
 rm dist/Linux/ISO/$lang/*.exe
 sed -e "s/ctrnl/ctr$lang/g" plugins/media/assets/citrine.sh > dist/Linux/ISO/$lang/citrine.sh
 chmod uog+x dist/Linux/ISO/$lang/citrine.sh
-tar cvzf "dist/Linux/OUT/$lang/citrine${lang}096.tar.gz" -C dist/Linux/ISO/ ${lang}
+tar cvzf "dist/Linux/OUT/$lang/citrine${lang}${VERSION}.tar.gz" -C dist/Linux/ISO/ ${lang}
 
 
 # Create Linux AppImage distribution
@@ -125,12 +112,11 @@ sed -e "s/ctrnl/ctr$lang/g" misc/Citrine.AppDir/AppRun > /tmp/${lang}/Citrine.Ap
 
 ./appimagetool-x86_64.AppImage /tmp/${lang}/Citrine.AppDir citrine_app ; cp citrine_app dist/Linux/ISO/${lang}/ctrapp_${lang}
 chmod uog+x dist/Linux/ISO/${lang}/ctrapp_${lang}
-sed -e "s/ctrnl/ctrapp_$lang/g" plugins/media/assets/citrine.sh > dist/Linux/ISO/$lang/citrine_app.sh
-sed -e "s/ctrapp_nl/ctrapp_$lang/g" dist/Linux/ISO/$lang/__7ai__ > dist/Linux/ISO/$lang/__7__ 
+#sed -e "s/ctrnl/ctrapp_$lang/g" plugins/media/assets/citrine.sh > dist/Linux/ISO/$lang/citrine_app.sh
 
 
-chmod uog+x dist/Linux/ISO/${lang}/citrine_app.sh
-tar cvzf "dist/Linux/OUT/$lang/citrine${lang}096ai.tar.gz" -C dist/Linux/ISO/ ${lang}
+#chmod uog+x dist/Linux/ISO/${lang}/citrine_app.sh
+tar cvzf "dist/Linux/OUT/$lang/citrine${lang}${VERSION}ai.tar.gz" -C dist/Linux/ISO/ ${lang}
 
 # Compile for Windows 32bit
 ISO="$lang" CC=i686-w64-mingw32-gcc-win32 DLLTOOL=i686-w64-mingw32-dlltool make -f makefile.win32 clean
