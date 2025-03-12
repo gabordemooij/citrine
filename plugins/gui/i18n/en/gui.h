8d3650080ff9a49f5e610516de3eceec18a91f86
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2025-03-12 12:31:08 +0100

    Update English GUI Plugin dictionary file.

--- plugins/gui/i18n/en/gui.h
+++ plugins/gui/i18n/en/gui.h
@@ -1,3 +1,4 @@
 #define CTR_DICT_GUI_PLUGIN_ID              "_GUI"
 #define CTR_DICT_XML_NAME_AT_SET            "xml:name:at:"
+#define CTR_DICT_NAME                       "name"
 #define CTR_DICT_FORM_FIELD_VALUE           "field-value:"
