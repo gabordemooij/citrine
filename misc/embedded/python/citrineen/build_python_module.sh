6d50073b19c36e9ca510037f65a6966f4d4f9947
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2025-04-06 23:53:27 +0200

    Prune project.

--- misc/embedded/python/citrineen/build_python_module.sh
+++
@@ -1,7 +0,0 @@
-ulimit -c unlimited
-rm -rf build/*
-rm -rf *.egg-info
-rm -rf *.so
-python3 setup.py clean
-python3 setup.py --verbose build
-cp build/lib.linux-x86_64-cpython-37/citrine_module_en.cpython-37m-x86_64-linux-gnu.so ../
