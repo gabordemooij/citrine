46c20c4fa2f218c99c8ca9332302c658c6b782f9
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2025-04-10 11:32:15 +0200

    Prune i18n.

--- src/plugins/gui/i18n/fr/guimsg.h
+++
@@ -1,4 +0,0 @@
-#define CTR_MSG_GUI_CONTEXTMENU_COPY       "copier"
-#define CTR_MSG_GUI_CONTEXTMENU_CUT        "couper"
-#define CTR_MSG_GUI_CONTEXTMENU_PASTE      "coller"
-#define CTR_MSG_GUI_CONTEXTMENU_SELALL     "sélectionner tout"
--- src/plugins/gui/i18n/nl/gui.h
+++
@@ -1,4 +0,0 @@
-#define CTR_DICT_GUI_PLUGIN_ID              "_GUI"
-#define CTR_DICT_XML_NAME_AT_SET            "xml:naam:bij:"
-#define CTR_DICT_NAME                       "naam"
-#define CTR_DICT_FORM_FIELD_VALUE           "invulwaarde:"
